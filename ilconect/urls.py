4f8eb5fd8d4f19a3bccdbd290bc0ef1af41b5108
Omar Alexis Gerardo Diaz Nuñez <omaralexis1313@gmail.com>
2023-02-21 10:55:14 -0700

    Initial commit

---
+++ ilconect/urls.py
@@ -0,0 +1,38 @@
+"""ilconect URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from inventory  import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+    path('singup/', views.singup),
+    path('login/', views.login),
+    path('inventory/', views.inventory),
+    path('products/', views.products),
+    path('products/new/', views.new_product),
+    path('products/modify/', views.modify_product),
+    path('suppliers/', views.suppliers),
+    path('suppliers/new/', views.new_supplier),
+    path('suppliers/modify/', views.modify_supplier),
+    path('categories/', views.categories),
+    path('categories/new/', views.new_category),
+    path('categories/modify/', views.modify_category),
+    path('users/', views.users),
+    path('users/modify/', views.modify_user),
+]
---
+++ inventory/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def singup(request):
+    if request.method == 'GET':
+        return render(request, 'singup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password1']:
+            try:
+                user = User.objects.create_user(
+                username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+        return HttpResponse('password is do not match')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def inventory(request):
+    return render(request, 'inventory.html')
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
+def new_product(request):
+    return render(request, 'new_product.html')
+
+
+def modify_product(request):
+    return render(request, 'modify_product.html')
+
+
+def suppliers(request):
+    return render(request, 'suppliers.html')
+
+
+def new_supplier(request):
+    return render(request, 'new_supplier.html')
+
+
+def modify_supplier(request):
+    return render(request, 'modify_supplier.html')
+
+
+def categories(request):
+    return render(request, 'categories.html')
+
+
+def new_category(request):
+    return render(request, 'new_category.html')
+
+
+def modify_category(request):
+    return render(request, 'modify_category.html')
+
+
+def users(request):
+    return render(request, 'users.html')
+
+
+def modify_user(request):
+    return render(request, 'modify_user.html')
