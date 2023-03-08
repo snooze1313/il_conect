a3976a68902289b842576a0ded0d2f9fcef992e7
Omar Alexis Gerardo Diaz Nuñez <omaralexis1313@gmail.com>
2023-03-07 17:42:09 -0700

    ya jala la hora
    la base de datos ya puede traer datos de fechas de otras tablas

--- inventory/models.py
+++ inventory/models.py
@@ -23,5 +23,6 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    date = models.DateTimeField(null=True, auto_now_add=True)
     def __str__(self):
         return self.name
